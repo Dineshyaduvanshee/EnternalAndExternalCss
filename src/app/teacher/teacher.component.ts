de28b74bd231bad246105ab82960e31c8acce7b0
Dinesh yadav <dineshyadav74950@gmail.com>
2024-01-12 10:45:17 +0530

    Style & StyleUrls In Angular

---
+++ src/app/teacher/teacher.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher',
+  templateUrl: './teacher.component.html',
+ // styleUrls: ['./teacher.component.css']
+// styles:['h1{background-color:yellow}']
+  styleUrls:['./teacher.component.css']
+})
+export class TeacherComponent {
+
+}
